7e79c47df205db0491b9f0df1f9894b0ec203447
dastonnazarov <dastonnazarov0910@gmail.com>
2023-03-30 16:17:09 +0500

    student all done

--- src/main/java/com/example/homework10/controller/BookController.java
+++ src/main/java/com/example/homework10/controller/BookController.java
@@ -6,7 +6,6 @@ import com.example.homework10.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
 
